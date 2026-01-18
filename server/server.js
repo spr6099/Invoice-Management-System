9900d7ceb13166b818f4928ad0a025f1837e080f
sandeep <sandeep.pr3@gmail.com>
2026-01-18 17:15:40 +0530

    for vercel

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ import customerRouter from "./routes/customerRoute.js";
 import itemRouter from "./routes/itemRoutes.js";
 import invoiceRouter from "./routes/invoiceRoutes.js";
 
-const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
@@ -25,4 +24,4 @@ app.get("/", (req, res) => {
   res.send("backend connected");
 });
 
-app.listen(PORT, () => console.log(`port connected on ${PORT}`));
+export default app
